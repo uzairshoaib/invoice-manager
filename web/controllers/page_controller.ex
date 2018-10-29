9f66ed5f8ce3af67ee8ac1895bcbea86a77e8731
Uzair Shoaib <uzairshoaib.dev@gmail.com>
2018-10-29 23:12:52 +0500

    plugged authentication required

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule InvoiceManager.PageController do
   use InvoiceManager.Web, :controller
-
+  import InvoiceManager.Auth, only: [authenticate_user: 2]
+  plug :authenticate_user
+  
   def index(conn, _params) do
     render conn, "index.html"
   end
