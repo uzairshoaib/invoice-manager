489bfb2394da6670e6ec3d79332f44fa5df9f52f
Uzair Shoaib <uzairshoaib.dev@gmail.com>
2018-10-29 23:09:37 +0500

    seed user

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,16 +10,20 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will halt execution if something goes wrong.
 
-alias InvoiceManager.InvoiceType;
-alias InvoiceManager.InvoiceItemUnit;
-alias InvoiceManager.Repo;
+alias InvoiceManager.InvoiceType
+alias InvoiceManager.InvoiceItemUnit
+alias InvoiceManager.Repo
+alias InvoiceManager.User
 
-invoice_types = ["Sale", "Purchase"]
-for invoice_type <- invoice_types do
-	Repo.get_by!(InvoiceType, title: invoice_type) || Repo.insert %InvoiceType{title: invoice_type}
-end
 
-invoice_item_units = ~w(KG)
-for invoice_item_unit <- invoice_item_units do
-	Repo.get_by(InvoiceItemUnit, title: invoice_item_unit) || Repo.insert %InvoiceItemUnit{title: invoice_item_unit}
-end
+Repo.insert %User{username: "admin", password_hash: Comeonin.Bcrypt.hashpwsalt("admin"), name: "Admin", email: "admin@invoicemanager.com"}
+
+# invoice_types = ["Sale", "Purchase"]
+# for invoice_type <- invoice_types do
+# 	Repo.get_by!(InvoiceType, title: invoice_type) || Repo.insert %InvoiceType{title: invoice_type}
+# end
+
+# invoice_item_units = ~w(KG)
+# for invoice_item_unit <- invoice_item_units do
+# 	Repo.get_by(InvoiceItemUnit, title: invoice_item_unit) || Repo.insert %InvoiceItemUnit{title: invoice_item_unit}
+# end
