905d5163ed87cc031dcca409bb64c96e96c62a66
Uzair Shoaib <uzairshoaib.dev@gmail.com>
2018-10-29 23:15:27 +0500

    added comeonin packae

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule InvoiceManager.Mixfile do
   def application do
     [
       mod: {InvoiceManager, []},
-      extra_applications: [:logger, :timex]
+      extra_applications: [:logger, :timex, :comeonin]
     ]
   end
 
@@ -41,7 +41,8 @@ defmodule InvoiceManager.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:timex, "~> 3.4"}
+      {:timex, "~> 3.4"},
+      {:comeonin, "~> 2.0"}
     ]
   end
 
