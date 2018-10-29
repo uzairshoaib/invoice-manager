74a176efe6d5d76dac6bcc2238e54dbc3705f5a4
Uzair Shoaib <uzairshoaib.dev@gmail.com>
2018-10-29 23:09:15 +0500

    added username to schema

--- web/models/user.ex
+++ web/models/user.ex
@@ -4,6 +4,7 @@ defmodule InvoiceManager.User do
   schema "users" do
     field :name, :string
     field :email, :string
+    field :username, :string
     field :password, :string, virtual: true
     field :password_hash, :string
 
